2116972b12c337aafc8d8dff9db192926bbe75fa
Sunggin Kim <sunggin.kim@stonybrook.edu>
2024-12-30 11:16:15 +0900

    updated

---
+++ includes/nav.php
@@ -0,0 +1,32 @@
+<header class="navbar-section">
+  <div class="navbar">
+    <i
+      class="fa fa-solid fa-bars hamburger-menu"
+      style="color: #f9f6f1"></i>
+    <div class="logo">
+      <a href="./index.html">
+        <img src="./img/Maison de Paris_Cream font_no bg.png" alt="logo" />
+      </a>
+    </div>
+    <nav>
+      <ul class="nav-links">
+        <li><a href="./about.html">About</a></li>
+        <li><a href="./menu.html">Menu</a></li>
+        <li><a href="./privateDining.html">Private Dining</a></li>
+        <li><a href="./highlights.html">Highlights</a></li>
+        <li>
+          <a
+            href="https://tr.ee/mMr_6bKW9t"
+            target="_blank"
+            rel="noopener noreferrer">Gift Cards</a>
+        </li>
+        <li><a href="./contact.html">Contact</a></li>
+      </ul>
+    </nav>
+    <a
+      href="https://app.catchtable.co.kr/ct/shop/lapero.ikseondong"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="btn">Book Now</a>
+  </div>
+</header>
